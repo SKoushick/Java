f911fef311fa7c0729267c10fccf8258eb03769e
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-08-13 06:44:24 +0530

    Update UserdefinedException.java

--- UserdefinedException.java
+++ UserdefinedException.java
@@ -1,4 +1,41 @@
-//USER DEFINED EXCEPTION
+//EXCEPTION HANDLING
+
+//TRY AND CATCH
+
+
+public class A{
+    public static void main(String args[]){
+        try{
+        int[] mynumber={1,2,3};
+        System.out.println(mynumber[11]);
+        }
+        catch(Exception e){
+            System.out.println("something went wrong"); 
+        }
+        finally{
+            System.out.println("this must be something else");
+        } 
+        }
+    }
+    
+
+//TRY THROW CATCH
+
+/* 
+import java.util.Scanner;
+public class A{
+    static void checkage(int age){
+        if (age>18){
+            throw new ArithmeticException("Access denied - your age is lower than 18");
+        }
+        else{
+            System.out.println("you are qualified for voting");
+        }
+    }
+    public static void main(String args[]){
+        checkage(19);
+    }
+}//USER DEFINED EXCEPTION
 /* 
 public class A{
     public static void main(String args[]){
