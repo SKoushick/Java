105788386b6a6c59aaf088077559a37278d0277d
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-08-13 06:29:03 +0530

    Create Generics.java

---
+++ Generics.java
@@ -0,0 +1,24 @@
+//GENERICS
+
+import java.util.TreeSet;
+
+public class A{
+    public static void main(String[] args){
+        int[] ar=new int[5];
+        ar[1]=2;
+        ar[0]=1;
+        /*ar[2]=3;
+        ar[3]=4;
+        ar[4]=5;
+        for(int i=0;i<=4;i++){
+            System.out.println(ar[i]);
+        }*/
+
+        TreeSet<Integer> ts=new TreeSet<Integer>();
+        ts.add(12);
+        ts.add(4);
+        ts.add(0);
+        
+        
+    }
+}
