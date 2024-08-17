0697e4096d88e2d54d92b0af9e96d1b6736e7a3f
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-08-17 17:19:56 +0530

    Create Enum.java

---
+++ Enum.java
@@ -0,0 +1,16 @@
+//ENUM EXAMPLE
+/
+enum level{
+    begginer,intermediate,advanced,expert;
+    private String abre;
+    level (String ab){
+        abre=ab;
+    }
+    StrigetAbre(){
+        return abre;
+    }
+    public class A{
+        
+    }
+}
+
