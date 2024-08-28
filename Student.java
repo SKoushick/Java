089c3ff5e13190f338f628cf2596643b8ffcf148
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-08-28 18:45:42 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,12 +6,12 @@ this.name = name;
 this.age = age;
 }
 public void displayInfo() {
-System.out.println(&quot;Name: &quot; + name);
-System.out.println(&quot;Age: &quot; + age);
+System.out.println("Name:" + name);
+System.out.println("Age: " + age);
 }
 public static void main(String[] args) {
-Student student1 = new Student(&quot;Alice&quot;, 20);
-Student student2 = new Student(&quot;Bob&quot;, 22);
+Student student1 = new Student("Alice", 20);
+Student student2 = new Student("Bob";, 22);
 student1.displayInfo();
 student2.displayInfo();
 
