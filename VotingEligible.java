2ecf6d6a5a5a5a2a4cc2534d26b00d173a89fa5d
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-08-28 18:54:01 +0530

    Create VotingEligible.java

---
+++ VotingEligible.java
@@ -0,0 +1,22 @@
+public class VoteEligibility {
+public static void main(String[] args) {
+int age = 25; // Set the person&#39;s age
+boolean isCitizen = true; // Set citizenship status
+// Check if the person is eligible to vote
+if (age &gt;= 18) { // Check age requirement
+
+if (isCitizen) { // Check citizenship status
+  System.out.println("You are eligible to vote");
+} 
+else {
+  System.ou.println("You must be a citizen");
+}
+}
+
+  else{
+  System.out.println("You must be a citizen");
+}
+}
+}
+  
+    
